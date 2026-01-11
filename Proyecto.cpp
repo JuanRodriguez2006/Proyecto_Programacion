78b8b6ebea69363f257192d294a257e33bf5dd39
JosueVargas24 <anthony.vargas01@epn.edu.ec>
2026-01-10 20:23:45 -0500

    Implementacion del vector y menu

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -45,12 +45,70 @@ void guardarTodo(vector<Producto>& productos) {
     }
 }
 
+vector<Producto> cargarProductos() {
+    vector<Producto> productos;
+    ifstream archivo(archivoDatos);
+
+    if (archivo.is_open()) {
+        string linea;
+        //Leer linea por linea hasta el final del archivo
+        while (getline(archivo,linea)) {
+            //Ignorar lineas en blanco o vacias
+            if (linea.empty()) {
+                continue;
+            }
+            //Parseo de la linea, tomar datos usando ";"como delimitador
+            vector<string>produ;
+            size_t pos = 0;
+            //Buscar cada ";" y extrae el texto antes de el
+            while ((pos = linea.find(";")) != string::npos) {
+                produ.push_back(linea.substr(0,pos)); //Extrae campo
+                linea.erase(0,pos+1); //Elimina lo extraido
+            }
+            produ.push_back(linea);
+            if (produ.size() == 4) {
+                Producto p;
+                p.codigo = stoi(produ[0]);
+                p.nombre = produ[1];
+                p.cantidad = stoi(produ[2]);
+                p.precio = stof(produ[3]);
+                productos.push_back(p);
+            }
+        }
+    }
+    return productos;
+}
 
 int main() {
     //Configurar consola para tildes y ñ
 
     system("chcp 65001 > nul");
     int opcion = 0;
+    do {
+	    cout << "\n---Sistema de Registro de Productos---\n";
+	    cout << "1. Agregar Producto\n";
+	    cout << "2. Ver lista de Productos\n";
+	    cout << "3. Actualizar Producto\n";
+	    cout << "4. Eliminar Producto\n";
+	    cout << "5. Salir\n";
+	    cout << "Seleccione una opción: ";
+	    cin >> opcion;
+	    cin.ignore();
+	    //Estructura de las opciones
+	    if (opcion == 1) {  
+	        
+	    }else if (opcion == 2) {
+	    	
+	    }else if (opcion == 3) {
 
+	    }else if (opcion == 4) {
+	    	
+	    }else if(opcion == 5){
+			cout<<"Saliendo del sistema...\n";
+		}else {
+			cout<<"Opción no válida. Ingresa un número del 1 al 4";
+	    }
+	    
+	} while (opcion != 5);
     return 0;
 }
